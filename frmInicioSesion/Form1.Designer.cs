acf51fd9bb620b92e42109ad8b4657bb2246b2b4
josLVR14 <Josevazrom6291@hotmail.com>
2024-11-21 06:38:10 -0600

    prueba formularios

---
+++ frmInicioSesion/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace frmInicioSesion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 450);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(310, 189);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(345, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "usuario";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(310, 277);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(345, 27);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "contraseña";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(310, 352);
+            button1.Name = "button1";
+            button1.Size = new Size(345, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Acceder";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(410, 68);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(165, 94);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private PictureBox pictureBox1;
+    }
+}
